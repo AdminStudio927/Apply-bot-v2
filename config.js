ae5e6d6611a15042daa897051e1857168b5317b3
Admin Studio <amoriamir66@gmail.com>
2025-02-16 11:30:00 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  token: 'توكن',
+  channels: {
+    applicationChannel: 'روم التقديم',
+    responseChannel: 'نتائج',
+    acceptedChannel: 'قبول و رفض',
+    banLogChannel: 'لوق حظر'
+  },
+  roles: {
+    accepted: 'مقبول بل ادارة',
+    rejected: 'مرفوض',
+    banned: 'بلاك ليست من الادارة',
+    staff: 'رتبت الاداري'
+  },
+  images: {
+    thumbnail: 'لوقو سيرفرك',
+    banner: 'بنر سيرفرك'
+  },
+  interviewServer: 'رابط انتظار مقابلة' 
+};
---
+++ index.js
@@ -0,0 +1,202 @@
+function _0x5a96(_0x2e9edd,_0x53b2f0){const _0x18b0e4=_0x18b0();return _0x5a96=function(_0x5a96f8,_0x30b852){_0x5a96f8=_0x5a96f8-0x9c;let _0x230ca4=_0x18b0e4[_0x5a96f8];return _0x230ca4;},_0x5a96(_0x2e9edd,_0x53b2f0);}const _0x2f8e28=_0x5a96;(function(_0x1483b3,_0x276682){const _0x45fd44=_0x5a96,_0x22ef7e=_0x1483b3();while(!![]){try{const _0x372b81=parseInt(_0x45fd44(0xa8))/0x1*(-parseInt(_0x45fd44(0xba))/0x2)+-parseInt(_0x45fd44(0xaa))/0x3*(-parseInt(_0x45fd44(0xa9))/0x4)+-parseInt(_0x45fd44(0xad))/0x5*(-parseInt(_0x45fd44(0xb6))/0x6)+-parseInt(_0x45fd44(0xa0))/0x7+-parseInt(_0x45fd44(0xa5))/0x8+-parseInt(_0x45fd44(0xbe))/0x9*(-parseInt(_0x45fd44(0xaf))/0xa)+parseInt(_0x45fd44(0xa3))/0xb;if(_0x372b81===_0x276682)break;else _0x22ef7e['push'](_0x22ef7e['shift']());}catch(_0x427832){_0x22ef7e['push'](_0x22ef7e['shift']());}}}(_0x18b0,0x321df));function _0x18b0(){const _0x597f96=['send','cache','GuildMembers','2425367jATUcG','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄▄\x20\x20\x0a\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▀███\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x0a\x20\x20\x20\x20\x20▄██▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄██▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20▄██▄\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20▄█▀██▄\x20\x20\x20▀████████▄█████▄\x20▀███\x20▀███▄███\x20\x20\x20\x20\x20\x20\x20\x20▄█▀██▄\x20\x20\x20▀████████▄\x20\x20\x20\x20▄█▀▀███\x20\x20\x20\x20\x20\x20\x20\x20\x20▄█▀██▄\x20\x20\x20▀████████▄\x20\x20\x20██▄████▄\x20\x20▄█▀██▄\x20▀███▄███▀███\x20\x20\x0a\x20\x20\x20▄█\x20\x20▀██\x20\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20██▀\x20▀▀\x20\x20\x20\x20\x20\x20\x20▄█\x20\x20▀██\x20\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20▄██\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20▄█\x20\x20▀██\x20\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20▀████\x20\x20\x20██\x20\x20\x20██▀\x20▀▀\x20\x20██\x20\x20\x0a\x20\x20\x20████████\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20████████\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20███\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20████████\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20\x20██\x20▄█████\x20\x20\x20██\x20\x20\x20\x20\x20\x20██\x20\x20\x0a\x20\x20█▀\x20\x20\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20█▀\x20\x20\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20██\x20\x20▀██\x20\x20\x20\x20██\x20\x20\x20\x20\x20\x20\x20█▀\x20\x20\x20\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20██\x20\x20\x20██▄\x20\x20\x20▄████\x20\x20\x20██\x20\x20\x20██\x20\x20\x20\x20\x20\x20██\x20\x20\x0a▄███▄\x20\x20\x20▄████▄████\x20\x20████\x20\x20████▄████▄████▄\x20\x20\x20\x20\x20\x20▄███▄\x20\x20\x20▄████▄████\x20\x20████▄\x20▀████▀███▄\x20\x20\x20▄███▄\x20\x20\x20▄████▄████\x20\x20████▄\x20█▀█████▀\x20▀████▀██▄████▄\x20\x20▄████▄\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Thank\x20you\x20for\x20trusting\x20Amir\x20And\x20Ali-Anbari\x20\x20and\x20A10\x20Studio.\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20discord.gg\x20/\x20\x0a','user','4755003TSOXfo','banner','379080ZMQXgP','setImage','applicationChannel','1YjkjFU','1208ccIdlD','1416xJFwxd','GuildMessages','channels','39935Gvrfmq','setTitle','5130DTtHtm','Guilds','setDescription','#0099ff','apply','Logged\x20in\x20as\x20','اضغط\x20على\x20الزر\x20أدناه\x20للتقديم','12waYMCv','التقديم\x20على\x20الإدارة','setStyle','setColor','782546vCtZPF','addComponents','./config.js','ready','7011wnZSVQ','images'];_0x18b0=function(){return _0x597f96;};return _0x18b0();}const {Client,GatewayIntentBits,EmbedBuilder,ActionRowBuilder,ButtonBuilder,ButtonStyle,ModalBuilder,TextInputBuilder,TextInputStyle}=require('discord.js'),config=require(_0x2f8e28(0xbc)),client=new Client({'intents':[GatewayIntentBits[_0x2f8e28(0xb0)],GatewayIntentBits[_0x2f8e28(0xab)],GatewayIntentBits[_0x2f8e28(0x9f)]]});client['on'](_0x2f8e28(0xbd),async()=>{const _0x155f47=_0x2f8e28;console['log'](_0x155f47(0xb4)+client[_0x155f47(0xa2)]['tag']+_0x155f47(0xa1));const _0x159ac7=client['channels'][_0x155f47(0x9e)]['get'](config[_0x155f47(0xac)][_0x155f47(0xa7)]);if(_0x159ac7){const _0x31a05d=new EmbedBuilder()[_0x155f47(0xae)](_0x155f47(0xb7))[_0x155f47(0xb1)](_0x155f47(0xb5))[_0x155f47(0xb9)](_0x155f47(0xb2))['setThumbnail'](config['images']['thumbnail'])[_0x155f47(0xa6)](config[_0x155f47(0x9c)][_0x155f47(0xa4)]),_0x5763f0=new ActionRowBuilder()[_0x155f47(0xbb)](new ButtonBuilder()['setCustomId'](_0x155f47(0xb3))['setLabel']('تقديم')[_0x155f47(0xb8)](ButtonStyle['Primary']));await _0x159ac7[_0x155f47(0x9d)]({'embeds':[_0x31a05d],'components':[_0x5763f0]});}});
+
+client.on('interactionCreate', async interaction => {
+  if (interaction.isButton()) {
+    if (interaction.customId === 'apply') {
+      // Check if user has banned role
+      if (interaction.member.roles.cache.has(config.roles.banned)) {
+        return interaction.reply({ 
+          content: 'عذراً، لا يمكنك التقديم حالياً', 
+          ephemeral: true 
+        });
+      }
+
+      const modal = new ModalBuilder()
+        .setCustomId('application_modal')
+        .setTitle('نموذج التقديم');
+
+      const nameInput = new TextInputBuilder()
+        .setCustomId('name')
+        .setLabel('ما اسمك؟')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+      const ageInput = new TextInputBuilder()
+        .setCustomId('age')
+        .setLabel('كم عمرك؟')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+      const idInput = new TextInputBuilder()
+        .setCustomId('discord_id')
+        .setLabel('ما هو الايدي الخاص بك؟')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+      const experienceInput = new TextInputBuilder()
+        .setCustomId('experience')
+        .setLabel('ما هي خبراتك؟')
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(true);
+
+      modal.addComponents(
+        new ActionRowBuilder().addComponents(nameInput),
+        new ActionRowBuilder().addComponents(ageInput),
+        new ActionRowBuilder().addComponents(idInput),
+        new ActionRowBuilder().addComponents(experienceInput)
+      );
+
+      await interaction.showModal(modal);
+    }
+
+    if (interaction.customId === 'accept' && interaction.member.roles.cache.has(config.roles.staff)) {
+      const userId = interaction.message.embeds[0].footer.text;
+      const member = await interaction.guild.members.fetch(userId);
+      
+      await member.roles.add(config.roles.accepted);
+      
+      const acceptEmbed = new EmbedBuilder()
+        .setTitle('تم قبول تقديمك!')
+        .setDescription('مرحباً بك في الفريق')
+        .setColor('#00ff00')
+        .setThumbnail(config.images.thumbnail)
+        .setImage(config.images.banner);
+
+      const acceptRow = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setLabel('انتظار مقابلة')
+            .setURL(config.interviewServer)
+            .setStyle(ButtonStyle.Link)
+        );
+
+      const channel = client.channels.cache.get(config.channels.acceptedChannel);
+      await channel.send({ content: `<@${userId}>`, embeds: [acceptEmbed], components: [acceptRow] });
+      
+      await interaction.message.edit({
+        components: [
+          new ActionRowBuilder()
+            .addComponents(
+              new ButtonBuilder()
+                .setCustomId('accepted_by')
+                .setLabel(`تم القبول بواسطة ${interaction.user.tag}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(true)
+            )
+        ]
+      });
+    }
+
+    if (interaction.customId === 'reject' && interaction.member.roles.cache.has(config.roles.staff)) {
+      const userId = interaction.message.embeds[0].footer.text;
+      const member = await interaction.guild.members.fetch(userId);
+      
+      await member.roles.add(config.roles.rejected);
+      
+      await interaction.message.edit({
+        components: [
+          new ActionRowBuilder()
+            .addComponents(
+              new ButtonBuilder()
+                .setCustomId('rejected_by')
+                .setLabel(`تم الرفض بواسطة ${interaction.user.tag}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(true)
+            )
+        ]
+      });
+    }
+
+    if (interaction.customId === 'ban' && interaction.member.roles.cache.has(config.roles.staff)) {
+      const userId = interaction.message.embeds[0].footer.text;
+      const member = await interaction.guild.members.fetch(userId);
+      
+      await member.roles.add(config.roles.banned);
+      
+      const banEmbed = new EmbedBuilder()
+        .setTitle('حظر من التقديم')
+        .setDescription(`تم حظر <@${userId}> من التقديم بواسطة <@${interaction.user.id}>`)
+        .setColor('#ff0000')
+        .setThumbnail(config.images.thumbnail)
+        .setImage(config.images.banner);
+
+      const banRow = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId(`unban_${userId}`)
+            .setLabel('إلغاء الحظر')
+            .setStyle(ButtonStyle.Danger)
+        );
+
+      const banLogChannel = client.channels.cache.get(config.channels.banLogChannel);
+      await banLogChannel.send({ embeds: [banEmbed], components: [banRow] });
+      
+      await interaction.message.edit({
+        components: [
+          new ActionRowBuilder()
+            .addComponents(
+              new ButtonBuilder()
+                .setCustomId('banned_by')
+                .setLabel(`تم الحظر بواسطة ${interaction.user.tag}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(true)
+            )
+        ]
+      });
+    }
+
+    if (interaction.customId.startsWith('unban_') && interaction.member.roles.cache.has(config.roles.staff)) {
+      const userId = interaction.customId.split('_')[1];
+      const member = await interaction.guild.members.fetch(userId);
+      
+      await member.roles.remove(config.roles.banned);
+      await member.send('تم إلغاء الحظر عنك. يمكنك التقديم مرة أخرى.');
+      
+      await interaction.message.edit({ components: [] });
+      await interaction.reply({ content: 'تم إلغاء الحظر بنجاح', ephemeral: true });
+    }
+  }
+
+  if (interaction.isModalSubmit() && interaction.customId === 'application_modal') {
+    const name = interaction.fields.getTextInputValue('name');
+    const age = interaction.fields.getTextInputValue('age');
+    const discordId = interaction.fields.getTextInputValue('discord_id');
+    const experience = interaction.fields.getTextInputValue('experience');
+
+    const applicationEmbed = new EmbedBuilder()
+      .setTitle('طلب تقديم جديد')
+      .setColor('#0099ff')
+      .setThumbnail(config.images.thumbnail)
+      .setImage(config.images.banner)
+      .addFields(
+        { name: 'الاسم', value: name },
+        { name: 'العمر', value: age },
+        { name: 'الايدي', value: discordId },
+        { name: 'الخبرات', value: experience }
+      )
+      .setFooter({ text: interaction.user.id });
+
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('accept')
+          .setLabel('قبول')
+          .setStyle(ButtonStyle.Success),
+        new ButtonBuilder()
+          .setCustomId('reject')
+          .setLabel('رفض')
+          .setStyle(ButtonStyle.Danger),
+        new ButtonBuilder()
+          .setCustomId('ban')
+          .setLabel('حظر')
+          .setStyle(ButtonStyle.Secondary)
+      );
+
+    const responseChannel = client.channels.cache.get(config.channels.responseChannel);
+    await responseChannel.send({ embeds: [applicationEmbed], components: [row] });
+    
+    await interaction.reply({ content: 'تم إرسال طلبك بنجاح!', ephemeral: true });
+  }
+});
+
+client.login(config.token);
